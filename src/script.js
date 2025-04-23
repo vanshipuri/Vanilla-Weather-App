da38c617dabfc4fec7c69065c0b0401373421206
Vanshi Puri <vanshipuri76@gmail.com>
2025-04-24 05:28:44 +0530

    added weather api

--- src/script.js
+++ src/script.js
@@ -1,9 +1,32 @@
+function updatetemperature(response) {
+   let temperatureElement = document.querySelector("#temperature");
+   let temperature = response.data.temperature.current;
+   let cityelement = document.querySelector("#searched-city");
+   
+   cityelement.innerHTML = response.data.city;
+  
+   
+    temperatureElement.innerHTML = Math.round(temperature);
+   }
+
+function searchcity(city) {
+    let apiKey = "b7oe38a4fa56t17674f3f0a7f3738e9e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(updatetemperature);
+}
+
+
+
+
 function handlesearchsubmit(event) {
 
     event.preventDefault();
     let cityinput = document.querySelector("#city-form-input");
-    let cityelement = document.querySelector("#searched-city");
-    cityelement.innerHTML = cityinput.value;
+   
+    searchcity(cityinput.value);
 }
 let searchformdocument = document.querySelector("#weather-form");
 searchformdocument.addEventListener("submit", handlesearchsubmit);
+
+
+searchcity("New York");
